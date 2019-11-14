5908a1b1c70c4625eb0060e9e17c2f5a8e3be2dc
mushfiq814 <mushfiq@mail.usf.edu>
2019-11-14 12:08:01 -0500

    commented environment.example.ts with instructions

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,3 +1,8 @@
+/**
+ * 1. Create src/environments/environment.ts
+ * 2. Paste the following
+ * 3. Replace 'YOUR_API_KEY' with your API Key from Google Dev Console
+ */
 export const environment = {
   production: false,
   youTubeUrl: 'https://www.googleapis.com/youtube/v3',
